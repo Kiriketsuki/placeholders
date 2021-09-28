33614dfb5e9dbe2543570244e6ec0900bef28d7f
kiriketsuki <kiriketsuki@gmail.com>
2021-09-28 21:50:50 +0800

    started flask

---
+++ website/views.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+
+views = Blueprint(__name__, "views")
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+@views.route("/base_template")
+def base_template():
+    render_template("base.html")
+
+@views.route("/profile")
+def profile():
+    args = request.args
+    username = args.get('username')
+    return render_template("index.html", username = username)
+
+@views.route("/go_to_home")
+def home_redirect():
+    return redirect(url_for("views.home"))
