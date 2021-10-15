2fa427a76aa594f320dbb3b49b040c3929fea061
CodingTeeRex <ivanpjh@gmail.com>
2021-10-15 16:35:22 +0800

    integrate preferences and profile to db

--- website/views.py
+++ website/views.py
@@ -12,6 +12,8 @@ from dateutil import parser
 views = Blueprint("views", __name__)
 
 # Landing page
+
+
 @views.route("/", methods=["GET", "POST"])
 def landing():
     if request.method == "POST":
@@ -26,13 +28,14 @@ def landing():
 
         # Check if credentials are valid
         # Check database for such user
-        user = User.query.filter_by(email = email).first()
-    
+        user = User.query.filter_by(email=email).first()
+
         # If user email exists
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in succesfully!', category='success')
-                login_user(user, remember=True) # remember allows user to stay logged in
+                # remember allows user to stay logged in
+                login_user(user, remember=True)
                 return redirect(url_for('views.recommendations', logged_in=True))
             else:
                 flash('Incorrect password, please try again.', category='error')
@@ -42,6 +45,8 @@ def landing():
     return render_template("landing.html", user=current_user)
 
 # Sign up page
+
+
 @views.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -50,36 +55,40 @@ def signup():
         email = request.form.get("email")
         password = request.form.get("password")
         confirmPassword = request.form.get("confirm-password")
-        
+
         # Error checking
         if firstName == None:
             flash('First name cannot be empty.', category='error')
         else:
             if hasDigit(firstName):
-                flash('First name must not contain numerical characters.', category='error')
+                flash('First name must not contain numerical characters.',
+                      category='error')
             elif len(firstName) < 2:
                 flash('First name is too short.', category='error')
-        
+
         if lastName == None:
             flash('Last name cannot be empty.', category='error')
         else:
             if hasDigit(lastName):
-                flash('Last name must not contain numerical characters.', category='error')
+                flash('Last name must not contain numerical characters.',
+                      category='error')
             elif len(lastName) < 2:
                 flash('Last name is too short.', category='error')
-        
+
         if len(email) < 10 or "@" not in email:
             flash('Not a valid email.', category='error')
-        
+
         if len(password) < 8:
             flash('Password too short.', category='error')
         elif not hasSpecialCharacters(password):
-            flash('Password must include at least one special character.', category='error')
+            flash('Password must include at least one special character.',
+                  category='error')
         elif password != confirmPassword:
             flash('Passwords do not match.', category='error')
         else:
             # add user to DB
-            newUser = User(firstName=firstName, lastName=lastName, email=email, password=generate_password_hash(password, method='sha256'))
+            newUser = User(firstName=firstName, lastName=lastName, email=email,
+                           password=generate_password_hash(password, method='sha256'))
             db.session.add(newUser)
 
             # check if account already exists
@@ -94,6 +103,9 @@ def signup():
 
     return render_template("sign_up.html", user=current_user)
 
+# Logout user
+
+
 @views.route('/logout')
 @login_required
 def logout():
@@ -101,20 +113,21 @@ def logout():
     flash('Logged out successfully!', category='success')
     return redirect(url_for('views.landing'))
 
-
 # Forgot password page
+
+
 @views.route("/forgot_password", methods=["GET", "POST"])
 def forgot_password():
     if request.method == "POST":
         email = request.POST.get('email')
 
         if email == None:
-            flash('Email required.', category='error') 
+            flash('Email required.', category='error')
 
         # Check if credentials are valid
         # Check database for such user
-        user = User.query.filter_by(email = email).first()
-    
+        user = User.query.filter_by(email=email).first()
+
         # If user email exists
         if not user:
             flash('Email does not exist.', category='error')
@@ -123,13 +136,9 @@ def forgot_password():
             # reset database with the new password
             # send email containing new password to user
             pass
-        
+
     return render_template("forgot_pw.html")
 
-# Homepage
-@views.route("/home")
-def home():
-    return render_template("home.html")
 
 @views.route("/account/settings", methods=["GET", "POST"])
 @login_required
@@ -140,18 +149,23 @@ def profile():
         if request.form.get('firstName') == "" and request.form.get('lastName') == "" and request.form.get('email') == "" and request.form.get('password') == "":
             flash("Empty fields.", category='error')
         else:
-            firstName = thisUser.firstName if request.form.get('firstName') == "" else request.form.get('firstName')
-            lastName = thisUser.lastName if request.form.get('lastName') == "" else request.form.get('lastName')
-            email = thisUser.email if request.form.get('email') == "" else request.form.get('email')
+            firstName = thisUser.firstName if request.form.get(
+                'firstName') == "" else request.form.get('firstName')
+            lastName = thisUser.lastName if request.form.get(
+                'lastName') == "" else request.form.get('lastName')
+            email = thisUser.email if request.form.get(
+                'email') == "" else request.form.get('email')
 
             # Check if credentials meet requirements
             if hasDigit(firstName):
-                flash('First name must not contain numerical characters.', category='error')
+                flash('First name must not contain numerical characters.',
+                      category='error')
             elif len(firstName) < 2:
                 flash('First name is too short.', category='error')
 
             if hasDigit(lastName):
-                flash('Last name must not contain numerical characters.', category='error')
+                flash('Last name must not contain numerical characters.',
+                      category='error')
             elif len(lastName) < 2:
                 flash('Last name is too short.', category='error')
 
@@ -159,34 +173,37 @@ def profile():
                 flash('Not a valid email.', category='error')
 
             if request.form.get('password') == "":
-                pwChanged = False # Boolean var to check if pw changed. False -> pw not changed
-                thisUser.password = thisUser.password 
-            else: 
-                pwChanged = True # Boolean var to check if pw changed. True -> pw changed
+                pwChanged = False  # Boolean var to check if pw changed. False -> pw not changed
+                thisUser.password = thisUser.password
+            else:
+                pwChanged = True  # Boolean var to check if pw changed. True -> pw changed
                 password = request.form.get('password')
                 if len(password) < 8:
                     flash('Password too short.', category='error')
                 elif not hasSpecialCharacters(password):
-                    flash('Password must include at least one special character.', category='error')
+                    flash(
+                        'Password must include at least one special character.', category='error')
                 else:
                     # Update user profile
-                    thisUser.password = generate_password_hash(password, method = 'sha256') if pwChanged else password
+                    thisUser.password = generate_password_hash(
+                        password, method='sha256') if pwChanged else password
 
             thisUser.firstName = firstName
             thisUser.lastName = lastName
             thisUser.email = email
 
-            print(firstName, lastName, email, pwChanged) # DEBUGGING
+            print(firstName, lastName, email, pwChanged)  # DEBUGGING
 
-            pwChanged = False # Reset
-            
+            pwChanged = False  # Reset
+
             db.session.commit()
-           
+
             flash('Profile updated!', category='success')
 
         print("Submit")
-        
-    return render_template("profile.html", user = current_user)
+
+    return render_template("profile.html", user=current_user)
+
 
 @views.route("/account/preferences", methods=["GET", "POST"])
 @login_required
@@ -195,30 +212,34 @@ def preferences():
         # Check if db has user preference already
         # db cannot hold 2 preferences under same uid as it is unique
         # if preference does not exist for user then add normally
-        # else update
-        print(current_user)
+        # else update db row for particular uid
+        print(current_user)  # DEBUGGING
 
-        thisPreference = Preference.query.filter_by(id=current_user.get_id()).first()
-        print(thisPreference)
+        thisPreference = Preference.query.filter_by(
+            id=current_user.get_id()).first()
+        print(thisPreference)  # DEBUGGING
 
-        attributes = {"houseType": None, "budget": None, "monthlyIncome": None, "maritalStatus": None, "cpf": None, "ownCar": None, "amenities": None, "preferredLocations": None}
-        
+        attributes = {"houseType": None, "budget": None, "monthlyIncome": None, "maritalStatus": None,
+                      "cpf": None, "ownCar": None, "amenities": None, "preferredLocations": None}
+
         attributes["houseType"] = request.form.get('typeOfHouse')
         attributes["budget"] = request.form.get('budget')
         attributes["monthlyIncome"] = request.form.get('monthlyIncome')
         attributes["maritalStatus"] = request.form.get('maritalStatus')
         attributes["cpf"] = request.form.get('cpfSavings')
-        attributes["ownCar"] = True if request.form.get('ownCar') == "Yes" else False
+        attributes["ownCar"] = True if request.form.get(
+            'ownCar') == "Yes" else False
         attributes["amenities"] = request.form.getlist('amenities')
         attributes["preferredLocations"] = request.form.getlist('locations')
 
         if None in attributes.values():
-            flash("Empty fields. All fields must be filled in.")
+            flash("Empty fields. All fields must be filled in.", category="error")
         else:
-            if thisPreference == None: # currently does not have preference hence can add to db
-                newPreference = Preference(houseType= attributes["houseType"], budget=attributes["budget"], monthlyIncome=attributes["monthlyIncome"], maritalStatus=attributes["maritalStatus"], cpf=attributes["cpf"], ownCar=attributes["ownCar"], amenities=attributes["amenities"], preferredLocations=attributes["preferredLocations"], uid=current_user.get_id())
+            if thisPreference == None:  # currently does not have preference hence can add to db
+                newPreference = Preference(houseType=attributes["houseType"], budget=attributes["budget"], monthlyIncome=attributes["monthlyIncome"], maritalStatus=attributes["maritalStatus"],
+                                           cpf=attributes["cpf"], ownCar=attributes["ownCar"], amenities=attributes["amenities"], preferredLocations=attributes["preferredLocations"], uid=current_user.get_id())
                 db.session.add(newPreference)
-            else:
+            else:  # has existing preference hence update row
                 thisPreference.houseType = attributes["houseType"]
                 thisPreference.budget = attributes["budget"]
                 thisPreference.monthlyIncome = attributes["monthlyIncome"]
@@ -228,28 +249,36 @@ def preferences():
                 thisPreference.amenities = attributes["amenities"]
                 thisPreference.preferredLocations = attributes["preferredLocations"]
 
-        print(attributes["houseType"], attributes["budget"], attributes["maritalStatus"], attributes["cpf"], attributes["ownCar"], attributes["amenities"], attributes["preferredLocations"]) # DEBUGGING
+            flash("Preferences updated!", category="success")
+
+        print(attributes["houseType"], attributes["budget"], attributes["maritalStatus"], attributes["cpf"],
+              attributes["ownCar"], attributes["amenities"], attributes["preferredLocations"])  # DEBUGGING
 
         db.session.commit()
 
-    return render_template("preferences.html", user = current_user)
+    return render_template("preferences.html", user=current_user)
+
 
 @views.route("/go_to_home")
 def home_redirect():
     return redirect(url_for("views.home"))
+
 
 @views.route("/recommendations")
 @login_required
 def recommendations():
     return render_template("top_picks_logged_in.html", user=current_user)
 
+
 @views.route('/recommendations/guest')
 def recommendations_guest():
     return render_template("top_picks_guest.html", user=current_user)
-        
+
+
 @views.route("/map")
 def map():
     return render_template("map.html", user=current_user)
+
 
 @views.route("/compare")
 @login_required
@@ -266,24 +295,28 @@ def compare():
 
 ######################################################################################
 
+
 @views.route("/csv")
 def csv():
-    return render_template("csv_chart.html", user = current_user)
+    return render_template("csv_chart.html", user=current_user)
+
 
 @views.route("/jovians_debug")
 def jovian():
-    user = User.query.filter_by(firstName ="admin").first()
+    user = User.query.filter_by(firstName="admin").first()
     for item in user.recommended:
         print(item.block)
     return f"{user.recommended[0].block}"
 
 # !temp fix to add buildings into csv
+
+
 @views.route("/import_buildings")
 def import_buildings():
     file = "website/gov_data.csv"
     cwd = os.getcwd()
-    data = pd.read_csv(os.path.normcase(os.path.join(cwd,file)))
-    test = data.head(5) # adding only 5
+    data = pd.read_csv(os.path.normcase(os.path.join(cwd, file)))
+    test = data.head(5)  # adding only 5
 
     for index, row in test.iterrows():
         id = row['_id']
@@ -299,27 +332,32 @@ def import_buildings():
         remaining_lease = row['remaining_lease']
         resale_price = row['resale_price']
 
-        new_building = building(id = id, month = month, town = town, flat_type = flat_type, block = block, street_name = street_name, storey_range = storey_range, floor_area_sqm = floor_area_sqm, flat_model = flat_model, lease_commence_date = lease_commence_date, resale_price = resale_price, remaining_lease = remaining_lease)
+        new_building = building(id=id, month=month, town=town, flat_type=flat_type, block=block, street_name=street_name, storey_range=storey_range,
+                                floor_area_sqm=floor_area_sqm, flat_model=flat_model, lease_commence_date=lease_commence_date, resale_price=resale_price, remaining_lease=remaining_lease)
         db.session.add(new_building)
         db.session.commit()
     return redirect(url_for('views.landing'))
 
+
 @views.route("/create_admin")
 def create_admin():
-    admin = User(firstName = "admin", lastName = "supreme", email = "testing@gmail.com", password = generate_password_hash("password", method = 'sha256'))
+    admin = User(firstName="admin", lastName="supreme", email="testing@gmail.com",
+                 password=generate_password_hash("password", method='sha256'))
     db.session.add(admin)
     db.session.commit()
 
     # I HAVENT TOOK CZ2007 IDK HOW TO DATABASE???
     # the above function imports 5 buildings so ill just add all 5 to my admin account
     # TODO something something select * from buildings something something add to user
-    for i in range(1,6):
-        temp_building = building.query.filter_by(id = i).first()
+    for i in range(1, 6):
+        temp_building = building.query.filter_by(id=i).first()
         temp_building.recommended_to.append(admin)
         db.session.commit()
     return redirect(url_for("views.landing"))
 
 # ! does not work because I HAVENT LEARNT DATABASES HOW DO I SQL?????
+
+
 @views.route("/testing_sql")
 def testing_sql():
     for building in db.session.query("building"):
