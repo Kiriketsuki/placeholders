d4bb99319fa324463d31796dd4a4c952917d79e3
CodingTeeRex <ivanpjh@gmail.com>
2021-10-15 14:13:11 +0800

    integrated preferences and profile to database

--- test.py
+++
@@ -1,5 +0,0 @@
-
-attributes = {"houseType": 100, "budget": None, "monthlyIncome": None, "maritalStatus": None, "cpf": None, "ownCar": None, "amenities": None, "preferredLocations": None}
-
-if None in attributes.values():
-    print("Empty fields. All fields must be filled in.")
