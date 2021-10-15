a577fbeadbfb7f6415034e9f8c220957dc0b327d
Restyled.io <commits@restyled.io>
2021-10-15 10:02:22 +0000

    Restyled by yapf

--- website/models.py
+++ website/models.py
@@ -50,7 +50,10 @@ class Preference(db.Model):
     ownCar = db.Column(db.Boolean)
     amenities = db.Column(db.JSON)
     preferredLocations = db.Column(db.JSON)
-    uid = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True, nullable=True)
+    uid = db.Column(db.Integer,
+                    db.ForeignKey("user.id"),
+                    unique=True,
+                    nullable=True)
 
 
 # building schema
