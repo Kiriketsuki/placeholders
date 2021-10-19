c1738f8b301b690c675e5956e29b3bcf16009a6f
Restyled.io <commits@restyled.io>
2021-10-19 12:37:22 +0000

    Restyled by prettier

--- website/static/scripts/add_favourites.js
+++ website/static/scripts/add_favourites.js
@@ -1,6 +1,8 @@
 function add_favourites(building_id) {
-  fetch('/add_favourites', {
-    method : "POST",
-    body : JSON.stringify({building_id : building_id})
-  }).then((_res) => {window.location.href = "#"})
+  fetch("/add_favourites", {
+    method: "POST",
+    body: JSON.stringify({ building_id: building_id }),
+  }).then((_res) => {
+    window.location.href = "#";
+  });
 }
