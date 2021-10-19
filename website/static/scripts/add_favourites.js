4be85bd92806332f5e946cfadbc7f1f93db6b421
Restyled.io <commits@restyled.io>
2021-10-19 12:37:15 +0000

    Restyled by clang-format

--- website/static/scripts/add_favourites.js
+++ website/static/scripts/add_favourites.js
@@ -1,8 +1,6 @@
 function add_favourites(building_id) {
-    fetch('/add_favourites', {
-        method: "POST",
-        body: JSON.stringify({building_id:building_id})
-    }).then((_res) => {
-        window.location.href = "#"
-    })
+  fetch('/add_favourites', {
+    method : "POST",
+    body : JSON.stringify({building_id : building_id})
+  }).then((_res) => {window.location.href = "#"})
 }
