ca40614c13af736ff7c10cec787d3220ca8d9b3b
Restyled.io <commits@restyled.io>
2021-10-29 06:44:23 +0000

    Restyled by prettier

--- website/static/scripts/add_favourites.js
+++ website/static/scripts/add_favourites.js
@@ -1,13 +1,17 @@
 function add_favourites(building_id) {
-  fetch('/add_favourites', {
-    method : "POST",
-    body : JSON.stringify({building_id : building_id})
-  }).then((_res) => {window.location.href = "#"})
+  fetch("/add_favourites", {
+    method: "POST",
+    body: JSON.stringify({ building_id: building_id }),
+  }).then((_res) => {
+    window.location.href = "#";
+  });
 }
 
 function remove_favourites(building_id) {
-  fetch('/remove_favourites', {
-    method : "POST",
-    body : JSON.stringify({building_id : building_id})
-  }).then((_res) => {window.location.href = "#"})
+  fetch("/remove_favourites", {
+    method: "POST",
+    body: JSON.stringify({ building_id: building_id }),
+  }).then((_res) => {
+    window.location.href = "#";
+  });
 }
